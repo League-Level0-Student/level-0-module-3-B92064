d935f9dae00cc826eeb28de842951f49c105dbec
B92064 <league@wts-im-6.attlocal.net>
2018-03-03 17:25:51 -0800

    RobotColorChooser, CrazyCatLady Finished

--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,14 +5,24 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+String cats = JOptionPane.showInputDialog("How many cats do you own?");
 		// 2. Convert their answer into an int
-
+int intcats = Integer.parseInt(cats);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-
+if (intcats >= 3) {
+	JOptionPane.showMessageDialog(null, "You are a crazy cat lady!");
+}
+else if(intcats < 3 && intcats > 0) {
+	playVideo("https://www.youtube.com/watch?v=5dsGWM5XGdg");
+}
+else if(intcats == 0) {
+	
+}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
 
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -2,18 +2,52 @@
 //    Level 0
 
 package elseif;
-
 import javax.swing.JOptionPane;
 import java.awt.Color;
-
+import org.jointheleague.graphical.robot.Robot;
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
-		
+		Robot cow = new Robot();
 		//3. Ask the user what color they would like the robot to draw
-		
+		for(int u = 0; u < 1000000; u ++) {
+		String color = JOptionPane.showInputDialog("What color would you like to draw with?");
 		//5. Use an if/else statement to set the pen color that the user requested
-
+		if(color.equalsIgnoreCase("green")){
+		cow.setPenColor(0, 255, 77);
+		}
+		else if(color.equalsIgnoreCase("blue")) {
+		cow.setPenColor(47, 84, 188);
+		}
+		else if(color.equalsIgnoreCase("red")) {
+			cow.setPenColor(148, 0, 0);
+			}
+		else if(color.equalsIgnoreCase("yellow")) {
+			cow.setPenColor(240, 230, 140);
+			}
+		else if(color.equalsIgnoreCase("pink")) {
+			cow.setPenColor(255, 47, 213);
+			}
+		else if(color.equalsIgnoreCase("black")) {
+			cow.setPenColor(0, 0, 0);
+			}
+		else if(color.equalsIgnoreCase("purple")) {
+			cow.setPenColor(123, 25, 188);
+			}
+		else if(color.equalsIgnoreCase("orange")) {
+			cow.setPenColor(255, 94, 0);
+			}
+		else if(color.equalsIgnoreCase("")) {
+			cow.setRandomPenColor();
+			}
+		cow.penDown();
+		cow.setSpeed(100);
+		cow.setPenWidth(10);
+		for (int i = 0; i < 5; i++){
+			cow.move(100);
+			cow.turn(40);
+		}
+		}
         //6. If the user doesn’t enter anything, choose a random color
 
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
